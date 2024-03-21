992d7d8459e5cebc752cecd27533ff81bcb4538e
beemaardhia <141271625+beemaardhia@users.noreply.github.com>
2024-03-21 17:05:44 +0700

    Update main.py

--- lemot/main.py
+++ lemot/main.py
@@ -1,7 +1,24 @@
 
 import nltk
-nltk.download('punkt')
-nltk.download('stopwords')
+import ssl
+
+# Membuat fungsi untuk memeriksa apakah koleksi data sudah terinstal
+def check_and_download(collection_name):
+    try:
+        nltk.data.find(collection_name)
+    except LookupError:
+        print(f"{collection_name} belum terinstal. Mengunduh koleksi data...")
+        nltk.download(collection_name)
+        print(f"{collection_name} berhasil diunduh.")
+    else:
+        print(f"{collection_name} sudah terinstal.")
+
+# Memeriksa dan mengunduh koleksi data 'punkt'
+check_and_download('punkt')
+
+# Memeriksa dan mengunduh koleksi data 'stopwords'
+check_and_download('stopwords')
+
 
 import pandas as pd
 import streamlit as st
